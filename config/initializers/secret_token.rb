68dfb771af532a231a64cdbd7fe1bfe2fe598b3f
Derek Tia <dtia05@gmail.com>
2012-12-16 22:43:51 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RenterCrm::Application.config.secret_token = 'dd4403ece22826632983b0bacda905988405de2c7ecaaddc2782fb6e9be0bd10fbc67f65f5f3f2c51988fdded147743a554ef0fe5ef1d1a7610b05a250263bab'
